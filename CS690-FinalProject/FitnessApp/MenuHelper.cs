ea2e2a6d74cb58a42193245d64965661a7ecf021
torasaur26 <victoria.wilson@bsu.edu>
2025-04-19 18:07:12 -0500

    Create MenuHelper.cs

---
+++ CS690-FinalProject/FitnessApp/MenuHelper.cs
@@ -0,0 +1,44 @@
+namespace FitnessApp
+{
+    public static class MenuHelper
+    {
+        public static int DisplayMenu(string title, string[] options)
+        {
+            int selected = 0;
+            ConsoleKey key;
+
+            do
+            {
+                Console.Clear();
+                Console.WriteLine(title + "\n");
+
+                for (int i = 0; i < options.Length; i++)
+                {
+                    if (i == selected)
+                    {
+                        Console.BackgroundColor = ConsoleColor.Gray;
+                        Console.ForegroundColor = ConsoleColor.Black;
+                        Console.WriteLine("> " + options[i]); 
+                        Console.ResetColor();
+                    }
+                    else
+                    {
+                        Console.WriteLine("  " + options[i]); 
+                    }
+                }
+
+                key = Console.ReadKey(true).Key;
+
+                // Navigation logic
+                if (key == ConsoleKey.UpArrow)
+                    selected = (selected == 0) ? options.Length - 1 : selected - 1;
+                else if (key == ConsoleKey.DownArrow)
+                    selected = (selected == options.Length - 1) ? 0 : selected + 1;
+
+            } while (key != ConsoleKey.Enter);
+
+            Console.Clear();
+            return selected; 
+        }
+    }
+}
